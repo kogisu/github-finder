1de9c713d946d17839cd19e9ff140a1a255f970f
e363517 <kent-ogisu@rms-ai-kent.ogisu-dev-1>
2019-06-08 20:22:05 -0400

    Initialized app with some components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import NavBar from './components/layout/NavBar';
+import Users from './components/users/Users';
+import './App.css'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      loading: true
+    };
+  }
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({
+      users: res.data,
+      loading: false
+    })
+  }
+  render() {
+    const { users, loading } = this.state;
+    return (
+      <div className="App">
+        <NavBar />
+        <Users loading={loading} users={users}/>
+      </div>
+    );
+  }
 }
 
-export default App;
+// export default App;
